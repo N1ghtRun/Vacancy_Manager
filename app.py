0c28295a072b9925d9c468d9eadaccf8cef14243
N1ghtRun <dmitro.shpanchuck@gmail.com>
2023-02-08 08:58:13 +0200

    Homework 3

--- app.py
+++ app.py
@@ -1,36 +1,68 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import db_processing
 import app_data
 
 app = Flask(__name__)
 
 
+@app.route('/', methods=['GET'])
+def main_page():
+    return render_template('main.html')
+
+
 @app.route('/vacancy/', methods=['GET', 'POST'])
 def all_vacancies():
-    return app_data.vacancy_data
+    if request.method == 'POST':
+        position_name = request.form.get('position_name')
+        company = request.form.get('company')
+        description = request.form.get('description')
+        contacts_ids = request.form.get('contacts_ids')
+        comment = request.form.get('comment')
+        vacancy_data = {'user_id': 1,
+                        'creation_date': '07.02.2023',
+                        'position_name': position_name,
+                        'company': company,
+                        'description': description,
+                        'contacts_ids': contacts_ids,
+                        'comment': comment,
+                        }
+        db_processing.insert_info('vacancy', vacancy_data)
+
+    result = db_processing.select_info("SELECT * FROM vacancy")
+    return render_template('vacancy_add.html', vacancies=result)
 
 
-@app.route('/vacancy/<vacancy_id>/', methods=['GET', 'PUT', 'DELETE'])
+@app.route('/vacancy/<int:vacancy_id>/', methods=['GET', 'PUT', 'DELETE'])
 def vacancy_single(vacancy_id):
-    for vacancy in app_data.vacancy_data:
-        if vacancy['id'] == vacancy_id:
-            return vacancy
+    result = db_processing.select_info(f"SELECT * FROM vacancy WHERE id == {vacancy_id}")
+    return render_template('single.html', object=result)
 
 
-@app.route('/vacancy/<vacancy_id>/events/', methods=['GET', 'POST'])
+@app.route('/vacancy/<int:vacancy_id>/events/', methods=['GET', 'POST'])
 def vacancy_events(vacancy_id):
-    event_list = []
-    for event in event_list:
-        if event['vacancy_id'] == vacancy_id:
-            event_list.append(event)
+    if request.method == 'POST':
+        description = request.form.get('description')
+        event_date = request.form.get('event_date')
+        title = request.form.get('title')
+        due_to_date = request.form.get('due_to_date')
+        event_data = {
+            'vacancy_id': vacancy_id,
+            'description': description,
+            'event_date': event_date,
+            'title': title,
+            'due_to_date': due_to_date,
+            'status': 0,
+        }
+        db_processing.insert_info('event', event_data)
 
-    return event_list
+    result = db_processing.select_info(f"SELECT * FROM event WHERE vacancy_id == {vacancy_id}")
+    return render_template('event_add.html', events=result, vacancy_id=vacancy_id)
 
 
-@app.route('/vacancy/<vacancy_id>/events/<event_id>/', methods=['GET', 'PUT', 'DELETE'])
+@app.route('/vacancy/<int:vacancy_id>/events/<int:event_id>/', methods=['GET', 'PUT', 'DELETE'])
 def vacancy_event_single(vacancy_id, event_id):
-    for event in app_data.event_data:
-        if event['id'] == event_id:
-            return event
+    result = db_processing.select_info(f"SELECT * FROM event WHERE id == {event_id}")
+    return render_template('single.html', object=result)
 
 
 @app.route('/vacancy/<id>/history', methods=['GET'])
